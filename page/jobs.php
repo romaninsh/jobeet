98ed10808885307a254605c50511223872cd3b36
Romans Malinovskis <r@agiletech.ie>
2011-04-26 03:13:44 +0100

    Revert "Added new routing for job pages"
    This should'nt be added till day 5
    This reverts commit 83b1bff550b0ee5f7a25686c5226e38d94d36d70.

--- page/jobs.php
+++ page/jobs.php
@@ -4,7 +4,7 @@ class page_jobs extends Page {
         parent::init();
         $p=$this;
 
-        $jobs = $this->add('JobList');
+        $jobs = $this->add('MVCGrid');
         $jobs->setModel('Job',array('location','position','company'));
         $jobs->addQuickSearch(array('location','position'));
         $jobs->addButton('Post a Job');
@@ -23,25 +23,5 @@ class page_jobs extends Page {
     }
     function defaultTemplate(){
         return array('page/jobs');
-    }
-}
-class JobList extends MVCGrid {
-    function format_link($field){
-
-        $parts=array(
-                $this->current_row['location'],
-                $this->current_row['company'],
-                $this->current_row['id'],
-                $this->current_row['position'],
-                );
-
-        $parts=preg_replace('/[^a-zA-Z 0-9-]/','',$parts);
-        $parts=preg_replace('/^$/','-',$parts);
-        $parts=str_replace(' ','_',$parts);
-        $page=implode('/',$parts);
-
-        $this->current_row[$field]='<a href="'.
-                    $this->api->getDestinationURL('job/'.$page).
-                    '">'.$this->current_row[$field].'</a>';
     }
 }
