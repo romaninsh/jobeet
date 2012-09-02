154deaab18a05fef0025723c3883bdc421eb5319
Romans Malinovskis <r@agiletech.ie>
2012-09-02 05:08:08 +0100

    4.2

--- page/jobs.php
+++ page/jobs.php
@@ -3,14 +3,14 @@ class page_jobs extends Page {
     function initMainPage(){
         $p=$this;
 
-        $jobs = $this->add('MVCGrid');
+        $jobs = $this->add('Grid');
         $jobs->setModel('Job',array('location','position','company'));
         $jobs->addQuickSearch(array('location','position'));
         $jobs->addButton('Post a Job');
 
         $jobs->addFormatter('company','link');
     }
-    function page_details(){
+    function page_company(){
         $v=$this->add('View',null,null,array('view/job_details'));
         $m=$v->setModel('Job')->loadData($_GET['id']);
         $v->template->del('has_logo');
